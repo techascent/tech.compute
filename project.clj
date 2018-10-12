89658888c06bcdcf58c5612278a256ede232244d
Chris Nuernberger <chris@techascent.com>
2018-10-12 14:53:41 -0600

    snap.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.compute "0.6.2"
+(defproject techascent/tech.compute "0.6.3-SNAPSHOT"
   :description "Library designed to provide a generic compute abstraction to allow some level of shared implementation between a cpu, cuda, openCL, webworkers, etc."
   :url "http://github.com/tech-ascent/tech.compute"
   :license {:name "Eclipse Public License"
