efd2353495dbf2755a62b21f982ed2948b83b14e
Chris Nuernberger <chris@thinktopic.com>
2018-10-14 13:24:57 -0600

    0.6.7

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.compute "0.6.7-SNAPSHOT"
+(defproject techascent/tech.compute "0.6.7"
   :description "Library designed to provide a generic compute abstraction to allow some level of shared implementation between a cpu, cuda, openCL, webworkers, etc."
   :url "http://github.com/tech-ascent/tech.compute"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.datatype "0.5.12"]
+                 [techascent/tech.datatype "0.5.13"]
                  [thinktopic/think.parallel "0.3.8"]
                  [thinktopic/think.resource "1.2.1"]
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]])
