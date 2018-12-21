a9de4c4fc16dca4a4c7817de7c5a50279f2b05bb
Chris Nuernberger <chris@thinktopic.com>
2018-12-21 10:02:43 -0700

    3.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject techascent/tech.compute "3.1-SNAPSHOT"
+(defproject techascent/tech.compute "3.1"
   :description "Library designed to provide a generic compute abstraction to allow some level of shared implementation between a cpu, cuda, openCL, webworkers, etc."
   :url "http://github.com/tech-ascent/tech.compute"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [techascent/tech.javacpp-datatype "3.0"]
-                 [techascent/tech.parallel "1.0"]
+                 [techascent/tech.parallel "1.1"]
                  ;;Backup for if we can't load system blas.  We then do array-backed
                  ;;nio buffers and use the netlib blas (most likely a lot slower).
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]])
