1fbb3090ae37b6e05be2288fa616911dbca6cb1f
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 15:52:06 -0700

    adding netlib back as fallback.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/tech-ascent/tech.compute"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[com.github.fommil.netlib/all "1.1.2" :extension "pom"]]
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]})
