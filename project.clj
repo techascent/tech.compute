39fcdafcf769e1ba4a875ab4306f140b4e5e206e
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 15:48:34 -0700

    Tried to move to deps.edn but pom-only dependencies aren't supported.

--- project.clj
+++ project.clj
@@ -3,9 +3,6 @@
   :url "http://github.com/tech-ascent/tech.compute"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.javacpp-datatype "3.0"]
-                 [techascent/tech.parallel "1.2"]
-                 ;;Backup for if we can't load system blas.  We then do array-backed
-                 ;;nio buffers and use the netlib blas (most likely a lot slower).
-                 [com.github.fommil.netlib/all "1.1.2" :extension "pom"]])
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
