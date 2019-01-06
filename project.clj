469940743aebc9b4a62f44047da125296f8311bd
Chris Nuernberger <chris@thinktopic.com>
2019-01-06 15:12:36 -0700

    added debugger support to launch config.

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[com.github.fommil.netlib/all "1.1.2" :extension "pom"]]
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]})
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :jvm-opts ["-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n"])
